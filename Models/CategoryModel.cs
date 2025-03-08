1d41f31c8468e105ecd478676adce4abf24a4cb9
Dino <woodming9889@gmail.com>
2025-03-09 03:32:05 +0800

    新增  Category Model

---
+++ Models/CategoryModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TeaTimeExample.Models
+{
+    /// <summary>
+    /// 類別
+    /// </summary>
+    public class CategoryModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
