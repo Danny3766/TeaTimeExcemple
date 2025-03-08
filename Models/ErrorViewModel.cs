e1efecd59d7bfe6e431d13e77a4da38d61d2015c
Dino <woodming9889@gmail.com>
2025-03-09 03:17:24 +0800

    init : 新建 asp.net Core MVC Web Application  (.net 8.0)

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TeaTimeExample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
