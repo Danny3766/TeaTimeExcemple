9a616221583720a589d7fd9f50065770b6550356
Dino <woodming9889@gmail.com>
2025-03-09 16:54:35 +0800

    新建 ApplicationDbContex.cs 設定連線的 Model

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TeaTimeExample.Models;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    // 自定義的資料表：
+    public DbSet<CategoryModel> CategoryModel { get; set; }
+}
