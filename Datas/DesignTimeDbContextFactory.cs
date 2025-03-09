4763edd35d320ca4ff886ddb26e7a6877947eee3
Dino <woodming9889@gmail.com>
2025-03-09 17:17:48 +0800

    chore : 新建 DesignTimeDbContextFactory 類別
    目的是在使用 套件管理器主控台搭配 dotnet ef 指令來建立遷移、更新資料庫等
    EF Core 會在執行這些操作時，尋找並使用此類別來創建 DbContext 實例

---
+++ Datas/DesignTimeDbContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        // 建立一個環境來讀取設定
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddUserSecrets<Program>() // 從 User Secrets 讀取
+            .Build();
+
+        // 取得 User Secrets 中的連線字串
+        var connectionString = builder.GetConnectionString("DefaultConnection");
+
+        // 建立 DbContext 並返回
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+        optionsBuilder.UseSqlServer(connectionString);
+
+        return new ApplicationDbContext(optionsBuilder.Options);
+    }
+}
