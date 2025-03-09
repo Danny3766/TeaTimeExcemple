39238a8c7593bba4229452d9b8667ed3faa8db70
Dino <woodming9889@gmail.com>
2025-03-09 21:32:32 +0800

    chore : Create( ) 使用 HttpPost 方式將頁面輸入的資料寫入 DB

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TeaTimeExample.Models;
 
 namespace TeaTimeExample.Controllers
 {
@@ -25,12 +26,26 @@ namespace TeaTimeExample.Controllers
         }
 
         /// <summary>
-        /// 新增類別
+        /// 新增類別 - 表單頁面
         /// </summary>
         /// <returns></returns>
         public IActionResult Create()
         {
             return View();
-        } 
+        }
+
+        /// <summary>
+        /// 新增類別 - 寫入資料到 DB
+        /// </summary>
+        /// <param name="category"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult Create(CategoryModel category)
+        {
+            _db.Categories.Add(category);
+            _db.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
