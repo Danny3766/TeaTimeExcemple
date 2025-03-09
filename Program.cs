9e6e6a3fe236b35b47eead45b76f94801b845dc2
Dino <woodming9889@gmail.com>
2025-03-09 18:13:44 +0800

    chore :  調整 Program.cs 的程式碼執行順序 及 稱加 環境判斷

--- Program.cs
+++ Program.cs
@@ -2,15 +2,21 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
 
 // 讀取 User Secrets 的連線字串設定
-builder.Configuration.AddUserSecrets<Program>();
+if (builder.Environment.IsDevelopment())
+{
+    builder.Configuration.AddUserSecrets<Program>();
+}
+
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 // 註冊 DbContext
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
