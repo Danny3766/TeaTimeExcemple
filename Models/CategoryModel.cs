f5252e5f3ae82e9d21a280b940a2d24b9f5d6fa4
Dino <woodming9889@gmail.com>
2025-03-09 15:36:54 +0800

    chore : 調整屬性的 Attribute

--- Models/CategoryModel.cs
+++ Models/CategoryModel.cs
@@ -9,7 +9,10 @@ namespace TeaTimeExample.Models
     {
         [Key]
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
         public int DisplayOrder { get; set; }
     }
 }
